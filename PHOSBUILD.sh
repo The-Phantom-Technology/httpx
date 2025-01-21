e47d16eeeadae6bac708d81ef5b4e16fed7a5f74
The Phantom Technology -Advanced Pentest <155259033+thephantomtechnology@users.noreply.github.com>
2025-01-21 15:27:41 +0800

    更新 PHOSBUILD.sh

--- PHOSBUILD.sh
+++ PHOSBUILD.sh
@@ -1,11 +1,36 @@
 rm -rf /opt/PHANTX/bin/httpx
 
-cd cmd/httpx
+make
 
-go build
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Make... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
 
 strip httpx
 
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Strip... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
 cp -Rf httpx /opt/PHANTX/bin/httpx
 
-chmod 755 /opt/PHANTX/bin/httpx 
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+chmod 755 /opt/PHANTX/bin/httpx
